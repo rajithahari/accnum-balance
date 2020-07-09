e8e1d8b4eb87e72e69a709aca980bd2e3bf62e2b
rajithahari <rajitha081189@gmail.com>
2020-07-09 20:22:17 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-// Import stylesheets
-import './style.css';
+acctData = [
+ {
+ "acctNum": "AAA - 1234",
+ "user": "Alice"
+ },
+ {
+ "acctNum": "AAA - 5231",
+ "user": "Bob"
+ },
+ {
+ "acctNum": "AAA - 9921",
+ "user": "Alice"
+ },
+ {
+ "acctNum": "AAA - 8191",
+ "user": "Alice"
+ }
+];
+balance = {
+ "AAA - 1234": 4593.22,
+ "AAA - 9921": 0,
+ "AAA - 5231": 232142.5,
+ "AAA - 8191": 4344
+};
+accNum = [];
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function getAccNumber(data, user, sortBy, sortByDirection) {
+  if (user) {
+    data.filter(index => {
+      if (user && index.user === 'Bob') {
+        accNum.push(index.acctNum);
+      } else if (user && index.user === 'Charlie') {
+        accNum.push(index.acctNum);
+      } else {
+        accNum.push(index.accNum);
+      }
+    });
+    return accNum;
+  }
+}
+
+console.log(getAccNumber(acctData, 'Bob')); // Return Account numbers having user Bob
+console.log(getAccNumber(acctData, 'Charlie')); // Return Account numbers having user Charlie
+console.log(getAccNumber(acctData, '', 'acctNum')); 
